e0188da1e24d598f1ef48390cfe223a457834a54
tierney12 <seantierney13@btinternet.com>
2021-02-09 17:04:24 +0000

    Added code

---
+++ code.php
@@ -0,0 +1,42 @@
+<body>
+
+
+<div id="dtext">
+  <h2>Click to retrieve the lastest update from the wind turbine inspection. </h2>
+  <button type="button" onclick="loadResults()">Get Results!</button>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<script>
+function loadResults() {
+    var content = <?php
+    
+            $string = "";
+					
+			for ($x = 1; $x <= 100; $x++){
+				if ($x % 5 == 0 && $x % 3 == 0){
+					$string = $string . "<p style='color:red'> Lightning Strike and Coating Damage </p>";
+				}
+				elseif ($x % 3 == 0){
+					$string = $string . "<p style='color:orange'> Coating Damage </p>";
+				}
+				elseif ($x % 5 == 0){
+					$string = $string . "<p style='color:blue'> Lightning Strike </p>";
+				}
+				else {$string = $string . "<p> {$x} </p>";}
+			}
+			
+			echo "{$string}";
+
+	?>;
+		
+   $("#dtext").html(content);
+}
+
+</script>
+
+
+
+</body>
+</html>
